896ee8e14ef7b9524d034e9018fb3bf41d378740
Columbus Instructor <instructor@wecancodeit.org>
2019-10-19 10:13:08 -0400

    Initial commit

---
+++ src/main/java/org/wcci/groupreviewssite/GroupReviewsSiteApplication.java
@@ -0,0 +1,13 @@
+package org.wcci.groupreviewssite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupReviewsSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupReviewsSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/wcci/groupreviewssite/GroupReviewsSiteApplicationTests.java
@@ -0,0 +1,16 @@
+package org.wcci.groupreviewssite;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GroupReviewsSiteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
